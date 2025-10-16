a404b599a157573bd33122a7190e5a6313a582cb
rominanafu <romina.najera.fuentes@gmail.com>
2025-10-16 15:55:48 -0600

    [ADD] IGAm with no validation of individuals in local and global search

---
+++ experiments/run_tests_IGAm.py
@@ -0,0 +1,19 @@
+from src.IGAm import solution
+from glob import glob
+import pandas as pd
+import re
+
+tests = ['C1', 'C2', 'R1', 'R2', 'RC1', 'RC2']
+vcap = [200, 700, 200, 1000, 200, 1000]
+for i in range(len(tests)):
+    t = tests[i]
+    cap = vcap[i]
+    ans = input(f"\nRun Solomon {t} tests? [y/n]: ")
+    if (ans.lower() == 'y'):
+        files_path = glob(f'data/solomon_dataset/{t}/*.csv')
+        for j in range(len(files_path)):
+            path = files_path[j]
+            instance = f'{t}{j+1:02d}'
+            ans = input(f"\nRun {instance} instance? [y/n]: ")
+            if (ans.lower() == 'y'):
+                solution(path, cap, instance)
